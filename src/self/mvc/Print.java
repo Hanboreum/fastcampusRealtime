eae15109d2c5e3245020764a943413bacdbf84ba
fullm <matoma1_@naver.com>
2024-03-19 18:25:17 +0900

    20240319
    개별, 전체 조회

--- src/self/mvc/Print.java
+++ src/self/mvc/Print.java
@@ -9,14 +9,24 @@ public class Print {
     public static void main(String[] args) {
         //view - controller - logic
         //1. 버튼 누르면 - 컨트롤러가 신호 받고 - 원하는 값(조회) 출력
-
         Controller controller = new Controller();
         Product product1 = new Product("keyboard",30000,"Computer");
         Product product2 = new Product("Almond",8000,"Food");
         Product result1 = controller.registerProducts(product1);
         Product result2 = controller.registerProducts(product2);
-        System.out.println(result2.toString() + " 등록 완료");
         //로직이 컨트롤로에게 받은걸 출력
+        System.out.println(result2.toString() + " 등록 완료");
+
+        //전체 항목 조회
+        List<Product> allProducts = controller.showAllProducts();
+        for (Product p:allProducts) {
+            System.out.println(p.toString());
+        }
+
+        //개별 조회
+        Product selectedProduct = controller.oneProduct(0);
+        System.out.println(selectedProduct.getName());
+
 
     }
 }
@@ -30,6 +40,15 @@ class Controller{
         return savedProduct;
     }
 
+    public List<Product> showAllProducts(){
+        List<Product> allProducts = logic.findAllProducts();
+        return allProducts;
+    }
+    public Product oneProduct(int id){
+        Product selectedProduct = logic.findOneProduct(id);
+        return selectedProduct;
+    }
+
 }
 
 class Logic{
@@ -40,5 +59,11 @@ class Logic{
     public Product addProduct(Product product){
         db.put(id,product);
         return db.get(id++);
+    }
+    public List<Product>findAllProducts(){
+        return new ArrayList<>(db.values());
+    }
+    public Product findOneProduct(int id){
+        return db.get(id);
     }
 }
